ba4f316914e4e778ab091b50699422680dad6bed
Martin Gondermann <magicmonty@pagansoft.de>
2015-08-22 22:49:08 +0200

    Adds base structure

---
+++ Option/Option.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Pagansoft.Functional
+{
+    public static class Option
+    {
+        public static Option<T> Some<T>(T value)
+        {
+            return new Option<T>.Some<T>(value);
+        }
+
+        public static Option<T> None<T>()
+        {
+            return new Option<T>.None<T>();
+        }
+    }
+
+    public abstract class Option<T>
+    {
+        public abstract bool IsSome { get; }
+
+        public bool IsNone { get { return !IsSome; } }
+
+        public abstract T Value { get; }
+
+        internal sealed class Some<V> : Option<V>
+        {
+            public Some(V value)
+            {
+                _value = value;
+            }
+
+            public override V Value { get { return _value; } }
+            private readonly V _value;
+
+            public override bool IsSome { get { return true; } }
+        }
+
+        internal sealed class None<V> : Option<V>
+        {
+            public override bool IsSome { get { return false; } }
+
+            public override V Value
+            {
+                get {
+                    throw new ArgumentException("A None Option has no value!");
+                }
+            }
+        }
+    }
+
+}
+
--- Test/OptionTest.cs
+++ Test/OptionTest.cs
@@ -1,15 +1,53 @@
 ﻿using NUnit.Framework;
 using System;
+using Shouldly;
 
-namespace Pagansoft.Option
+namespace Pagansoft.Functional
 {
-	[TestFixture ()]
-	public class Test
-	{
-		[Test ()]
-		public void TestCase ()
+	[TestFixture]
+    public class OptionTest
+    {
+        [Test]
+        public void IsSome_Is_True_For_Option_Some()
 		{
+            var sut = Option.Some<int>(1);
+            sut.IsSome.ShouldBe(true);
 		}
+
+        [Test]
+        public void IsNone_Is_False_For_Option_Some()
+        {
+            var sut = Option.Some<int>(1);
+            sut.IsNone.ShouldBe(false);
+        }
+
+        [Test]
+        public void Value_Of_Some_Should_Be_Value_From_Constructor()
+        {
+            var sut = Option.Some<int>(1);
+            sut.Value.ShouldBe(1);
+        }
+
+        [Test]
+        public void IsSome_Is_False_For_Option_None()
+        {
+            var sut = Option.None<int>();
+            sut.IsSome.ShouldBe(false);
+        }
+
+        [Test]
+        public void IsNone_Is_True_For_Option_None()
+        {
+            var sut = Option.None<int>();
+            sut.IsNone.ShouldBe(true);
+        }
+
+        [Test]
+        public void Value_Of_None_Should_Throw_Exception()
+        {
+            var sut = Option.None<int>();
+            Should.Throw<ArgumentException>(() => { var x = sut.Value; }).Message.ShouldBe("A None Option has no value!");
+        }
 	}
 }
-
+    
