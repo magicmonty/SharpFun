fdb8dc9e73b7d5173c6343594e726154e8531b94
Martin Gondermann <magicmonty@pagansoft.de>
2015-08-22 23:33:00 +0200

    Implements the base functionality of Option

--- Option/Option.cs
+++ Option/Option.cs
@@ -2,50 +2,144 @@
 
 namespace Pagansoft.Functional
 {
-    public static class Option
+    /// <summary>Base class for the Option Monad</summary>
+    public abstract class Option<T> : IEquatable<Option<T>>
     {
-        public static Option<T> Some<T>(T value)
+        /// <summary>Gets a value indicating whether this instance represents some value.</summary>
+        /// <value><c>true</c> if this instance represents some value; otherwise, <c>false</c>.</value>
+        public abstract bool IsSome { get; }
+
+        /// <summary>Gets a value indicating whether this instance represents no value.</summary>
+        /// <value><c>true</c> if this instance represents no value; otherwise, <c>false</c>.</value>
+        public bool IsNone { get { return !IsSome; } }
+
+        /// <summary>Gets the value.</summary>
+        public abstract T Value { get; }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="Pagansoft.Functional.Option`1[[`0]]"/> is equal to the current <see cref="Pagansoft.Functional.Option`1"/>.
+        /// </summary>
+        /// <param name="other">The <see cref="Pagansoft.Functional.Option`1[[`0]]"/> to compare with the current <see cref="Pagansoft.Functional.Option`1"/>.</param>
+        /// <returns><c>true</c> if the specified <see cref="Pagansoft.Functional.Option`1[[`0]]"/> is equal to the current
+        /// <see cref="Pagansoft.Functional.Option`1"/>; otherwise, <c>false</c>.</returns>
+        public abstract bool Equals(Option<T> other);
+
+        /// <summary>
+        /// Returns the value or.
+        /// </summary>
+        /// <returns>The value or.</returns>
+        /// <param name="defaultValue">Default value.</param>
+        public abstract T ReturnValueOr(T defaultValue);
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object"/> is equal to the current <see cref="Pagansoft.Functional.Option`1"/>.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object"/> to compare with the current <see cref="Pagansoft.Functional.Option`1"/>.</param>
+        /// <returns><c>true</c> if the specified <see cref="System.Object"/> is equal to the current
+        /// <see cref="Pagansoft.Functional.Option`1"/>; otherwise, <c>false</c>.</returns>
+        public override bool Equals(object obj)
         {
-            return new Option<T>.Some<T>(value);
+            return Equals(obj as Option<T>);
         }
 
-        public static Option<T> None<T>()
+        /// <summary>
+        /// Serves as a hash function for a <see cref="Pagansoft.Functional.Option`1"/> object.
+        /// </summary>
+        /// <returns>A hash code for this instance that is suitable for use in hashing algorithms and data structures such as a
+        /// hash table.</returns>
+        public override int GetHashCode()
         {
-            return new Option<T>.None<T>();
+            return 0;
+        }
+
+        /// <param name="value">Value.</param>
+        public static implicit operator Option<T>(T value) 
+        {
+            return ReferenceEquals(null, value)
+                ? Option.None<T>()
+                : Option.Some(value);
         }
     }
 
-    public abstract class Option<T>
+    /// <summary>Factory class for fluent construction of an option monad</summary>
+    public static class Option
     {
-        public abstract bool IsSome { get; }
-
-        public bool IsNone { get { return !IsSome; } }
-
-        public abstract T Value { get; }
-
-        internal sealed class Some<V> : Option<V>
+        /// <summary>Represents some result</summary>
+        private sealed class Some<TResult> : Option<TResult>
         {
-            public Some(V value)
+            public Some(TResult value)
             {
                 _value = value;
             }
 
-            public override V Value { get { return _value; } }
-            private readonly V _value;
+            public override TResult Value { get { return _value; } }
+            private readonly TResult _value;
 
             public override bool IsSome { get { return true; } }
+
+            public override TResult ReturnValueOr(TResult defaultValue) 
+            {
+                return _value;
+            }
+
+            public override bool Equals(Option<TResult> other)
+            {
+                if (ReferenceEquals(null, other) || other.GetType() != this.GetType())
+                    return false;
+                
+                if (ReferenceEquals(this, other))
+                    return true;
+
+                return Equals(_value, other.Value);
+            }
+
+            public override int GetHashCode()
+            {
+                return _value != null ? _value.GetHashCode() : 0;
+            }
         }
 
-        internal sealed class None<V> : Option<V>
+        private sealed class None<TResult> : Option<TResult>
         {
             public override bool IsSome { get { return false; } }
 
-            public override V Value
+            public override TResult Value
             {
                 get {
                     throw new ArgumentException("A None Option has no value!");
                 }
             }
+
+            public override TResult ReturnValueOr(TResult defaultValue) 
+            {
+                return defaultValue;
+            }
+
+            public override bool Equals(Option<TResult> other)
+            {
+                return !ReferenceEquals(null, other) 
+                    && other.GetType() == this.GetType();
+            }
+
+            public override int GetHashCode()
+            {
+                return 0;
+            }
+        }
+
+        /// <summary>Creates an option which represents the specified value.</summary>
+        /// <param name="value">The value, this option represent.</param>
+        /// <typeparam name="T">The type of the represented value.</typeparam>
+        public static Option<T> Some<T>(T value)
+        {
+            return new Some<T>(value);
+        }
+
+        /// <summary>Creates an option which represents no value of a given type.</summary>
+        /// <typeparam name="T">The type of the represented value.</typeparam>
+        public static Option<T> None<T>()
+        {
+            return new None<T>();
         }
     }
 
--- Test/OptionTest.cs
+++ Test/OptionTest.cs
@@ -4,27 +4,27 @@ using Shouldly;
 
 namespace Pagansoft.Functional
 {
-	[TestFixture]
+    [TestFixture]
     public class OptionTest
     {
         [Test]
         public void IsSome_Is_True_For_Option_Some()
-		{
-            var sut = Option.Some<int>(1);
+        {
+            var sut = Option.Some(1);
             sut.IsSome.ShouldBe(true);
-		}
+        }
 
         [Test]
         public void IsNone_Is_False_For_Option_Some()
         {
-            var sut = Option.Some<int>(1);
+            var sut = Option.Some(1);
             sut.IsNone.ShouldBe(false);
         }
 
         [Test]
         public void Value_Of_Some_Should_Be_Value_From_Constructor()
         {
-            var sut = Option.Some<int>(1);
+            var sut = Option.Some(1);
             sut.Value.ShouldBe(1);
         }
 
@@ -46,8 +46,86 @@ namespace Pagansoft.Functional
         public void Value_Of_None_Should_Throw_Exception()
         {
             var sut = Option.None<int>();
-            Should.Throw<ArgumentException>(() => { var x = sut.Value; }).Message.ShouldBe("A None Option has no value!");
+            Should.Throw<ArgumentException>(() => {
+                var x = sut.Value;
+            }).Message.ShouldBe("A None Option has no value!");
         }
-	}
+
+        [Test]
+        public void Option_Some_Are_Equal_If_Content_Is_Equal()
+        {
+            var option1 = Option.Some("FOO");
+            var option2 = Option.Some("FOO");
+
+            Equals(option1, option2).ShouldBe(true);
+            option1.Equals(option2).ShouldBe(true);
+            option2.Equals(option1).ShouldBe(true);
+            option1.ShouldBe(option2);
+            option1.GetHashCode().ShouldBe(option2.GetHashCode());
+        }
+
+        [Test]
+        public void Option_Some_Are_Not_Equal_If_Content_Is_Not_Equal()
+        {
+            var option1 = Option.Some("FOO");
+            var option2 = Option.Some("BAR");
+
+            Equals(option1, option2).ShouldBe(false);
+            option1.Equals(option2).ShouldBe(false);
+            option2.Equals(option1).ShouldBe(false);
+            option1.ShouldNotBe(option2);
+        }
+
+        [Test]
+        public void Option_None_Are_Equal_If_Containing_Type_Is_Same() 
+        {
+            var option1 = Option.None<int>();
+            var option2 = Option.None<int>();
+
+            Equals(option1, option2).ShouldBe(true);
+            option1.Equals(option2).ShouldBe(true);
+            option2.Equals(option1).ShouldBe(true);
+            option1.ShouldBe(option2);
+            option1.GetHashCode().ShouldBe(option2.GetHashCode());
+        }
+
+        [Test]
+        public void Option_None_And_Option_Some_Are_Not_Equal()
+        {
+            var option1 = Option.Some<int>(1);
+            var option2 = Option.None<int>();
+
+            option1.ShouldNotBe(option2);
+            option2.ShouldNotBe(option1);
+        }
+
+        [Test]
+        public void Implicit_Conversion_Converts_To_A_Some_Option() 
+        {
+            Option<int> actual = 1;
+
+            actual.ShouldBe(Option.Some(1));
+        }
+
+        [Test]
+        public void Implicit_Conversion_Of_Null_Converts_To_A_None_Option() 
+        {
+            Option<string> actual = (string)null;
+
+            actual.ShouldBe(Option.None<string>());
+        }
+
+        [Test]
+        public void ReturnValueOr_Of_A_Some_Returns_Its_Value()
+        {
+            Option.Some("FOO").ReturnValueOr("BAR").ShouldBe("FOO");
+        }
+
+        [Test]
+        public void ReturnValueOr_Of_A_None_Returns_Its_Value()
+        {
+            Option.None<string>().ReturnValueOr("BAR").ShouldBe("BAR");
+        }
+    }
 }
     
