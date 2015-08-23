0258d7b99bcebecfd306efe53b2fd527406ac097
Martin Gondermann <magicmonty@pagansoft.de>
2015-08-23 13:17:25 +0200

    Implements Extension methods Do and OtherwiseDo

---
+++ Option/OptionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace Pagansoft.Functional
+{
+    public static class OptionExtensions
+    {
+        /// <summary>
+        /// Invokes the given <paramref name="action"/> with the value of the given <paramref name="option"/>
+        /// if the <paramref name="option"/> has a value.
+        /// </summary>
+        /// <param name="option">The option.</param>
+        /// <param name="action">The action to invoke.</param>
+        /// <typeparam name="T">The type of the option value.</typeparam>
+        public static void Do<T>(this Option<T> option, Action<T> action)
+        {
+            Contract.Requires(option != null);
+            Contract.Requires(action != null);
+
+            if (option.IsSome)
+                action(option.Value);
+        }
+
+        /// <summary>
+        /// Invokes the given <paramref name="action"/>
+        /// if the given <paramref name="option"/> has no value.
+        /// </summary>
+        /// <param name="option">The option.</param>
+        /// <param name="action">The action to invoke.</param>
+        /// <param name="action">Action.</param>
+        public static void OtherwiseDo<T>(this Option<T> option, Action action)
+        {
+            Contract.Requires(option != null);
+            Contract.Requires(action != null);
+
+            if (option.IsNone)
+                action();
+        }
+    }
+}
+
---
+++ Test/OptionExtensionsTest.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System;
+using Shouldly;
+
+namespace Pagansoft.Functional
+{
+    [TestFixture]
+    public class OptionExtensionsTest
+    {
+        [Test]
+        public void Do_Executes_If_Option_Contains_Value()
+        {
+            var wasExecuted = string.Empty;
+
+            var option = Option.Some("FOO");
+
+            option.Do(v => wasExecuted = string.Format("{0} was executed", v));
+
+            wasExecuted.ShouldBe("FOO was executed");
+        }
+
+        [Test]
+        public void Do_Does_Not_Execute_If_Option_Contains_No_Value()
+        {
+            var wasExecuted = string.Empty;
+            var option = Option.None<string>();
+
+            option.Do(v => wasExecuted = string.Format("{0} was executed", v));
+
+            wasExecuted.ShouldBeEmpty();
+        }
+
+        [Test]
+        public void OtherwiseDo_Executes_If_Option_Contains_No_Value()
+        {
+            var wasExecuted = false;
+            var option = Option.None<string>();
+
+            option.OtherwiseDo(() => wasExecuted = true);
+
+            wasExecuted.ShouldBe(true);
+        }
+
+        [Test]
+        public void OtherwiseDo_Does_Not_Execute_If_Option_Contains_Value()
+        {
+            var wasExecuted = false;
+            var option = Option.Some("FOO");
+
+            option.OtherwiseDo(() => wasExecuted = true);
+
+            wasExecuted.ShouldBe(false);
+        }
+    }
+}
+
