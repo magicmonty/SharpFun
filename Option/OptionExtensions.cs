fc5f6ed84ee89edf0a37ffec1189aa02efd16291
Martin Gondermann <magicmonty@pagansoft.de>
2015-08-23 13:37:10 +0200

    Implements the Match extension methods

--- Option/OptionExtensions.cs
+++ Option/OptionExtensions.cs
@@ -7,35 +7,77 @@ namespace Pagansoft.Functional
     public static class OptionExtensions
     {
         /// <summary>
-        /// Invokes the given <paramref name="action"/> with the value of the given <paramref name="option"/>
+        /// Invokes the given <paramref name="someAction"/> with the value of the given <paramref name="option"/>
         /// if the <paramref name="option"/> has a value.
         /// </summary>
         /// <param name="option">The option.</param>
-        /// <param name="action">The action to invoke.</param>
+        /// <param name="someAction">The action to invoke.</param>
         /// <typeparam name="T">The type of the option value.</typeparam>
-        public static void Do<T>(this Option<T> option, Action<T> action)
+        public static Option<T> Do<T>(this Option<T> option, Action<T> someAction)
         {
             Contract.Requires(option != null);
-            Contract.Requires(action != null);
+            Contract.Requires(someAction != null);
+            Contract.Ensures(Contract.Result<Option<T>>() != null);
 
             if (option.IsSome)
-                action(option.Value);
+                someAction(option.Value);
+
+            return option;
         }
 
         /// <summary>
-        /// Invokes the given <paramref name="action"/>
+        /// Invokes the given <paramref name="noneAction"/>
         /// if the given <paramref name="option"/> has no value.
         /// </summary>
         /// <param name="option">The option.</param>
-        /// <param name="action">The action to invoke.</param>
-        /// <param name="action">Action.</param>
-        public static void OtherwiseDo<T>(this Option<T> option, Action action)
+        /// <param name="noneAction">The action to invoke.</param>
+        /// <param name="noneAction">Action.</param>
+        public static Option<T> OtherwiseDo<T>(this Option<T> option, Action noneAction)
         {
             Contract.Requires(option != null);
-            Contract.Requires(action != null);
+            Contract.Requires(noneAction != null);
+            Contract.Ensures(Contract.Result<Option<T>>() != null);
 
             if (option.IsNone)
-                action();
+                noneAction();
+            
+            return option;
+        }
+
+        /// <summary>
+        /// Runs the specified <paramref name="action"/> if the <paramref name="option"/> matches.
+        /// </summary>
+        /// <param name="option">Option.</param>
+        /// <param name="action">The action to run, if the option matches a Some.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public static Option<T> Match<T>(this Option<T> option, Action<T> action)
+        {
+            return option.Do(action);
+        }
+
+        /// <summary>
+        /// Runs the specified <paramref name="action"/> if the <paramref name="option"/> matches.
+        /// </summary>
+        /// <param name="option">Option.</param>
+        /// <param name="action">The action to run, if the option matches a None.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public static Option<T> Match<T>(this Option<T> option, Action action)
+        {
+            return option.OtherwiseDo(action);
+        }
+
+        /// <summary>
+        /// Runs the matching action for the given <paramref name="option"/>.
+        /// </summary>
+        /// <param name="option">Option.</param>
+        /// <param name="someAction">The action to run, if the option matches a Some.</param>
+        /// <param name="noneAction">The action to run, if the option matches a None.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public static Option<T> Match<T>(this Option<T> option, Action<T> someAction, Action noneAction)
+        {
+            return option
+                .Do(someAction)
+                .OtherwiseDo(noneAction);
         }
     }
 }
--- Test/OptionExtensionsTest.cs
+++ Test/OptionExtensionsTest.cs
@@ -51,6 +51,55 @@ namespace Pagansoft.Functional
 
             wasExecuted.ShouldBe(false);
         }
+
+        [Test]
+        public void Match_With_A_Parametrized_Action_Is_The_Same_As_Do()
+        {
+            var wasExecuted = string.Empty;
+
+            var option = Option.Some("FOO");
+
+            option.Match(v => wasExecuted = string.Format("{0} was executed", v));
+
+            wasExecuted.ShouldBe("FOO was executed");
+        }
+
+        [Test]
+        public void Match_With_An_Unparametrized_Action_Is_The_Same_As_OtherwiseDo()
+        {
+            var wasExecuted = string.Empty;
+            var option = Option.None<string>();
+
+            option.Match(() => wasExecuted = "was executed");
+
+            wasExecuted.ShouldBe("was executed");
+        }
+
+        [Test]
+        public void Match_With_Two_Actions_Is_Same_As_Do_And_Otherwise_Do_Combined_For_None_Option()
+        {
+            var wasExecuted = string.Empty;
+            var option = Option.None<string>();
+
+            option.Match(
+                v => wasExecuted = string.Format("{0} was executed", v), 
+                () => wasExecuted = "None was executed");
+
+            wasExecuted.ShouldBe("None was executed");
+        }
+
+        [Test]
+        public void Match_With_Two_Actions_Is_Same_As_Do_And_Otherwise_Do_Combined_For_Some_Option()
+        {
+            var wasExecuted = string.Empty;
+            var option = Option.Some("FOO");
+
+            option.Match(
+                v => wasExecuted = string.Format("{0} was executed", v), 
+                () => wasExecuted = "None was executed");
+
+            wasExecuted.ShouldBe("FOO was executed");
+        }
     }
 }
 
