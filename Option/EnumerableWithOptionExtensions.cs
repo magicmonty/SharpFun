5502b6c0b790f77672e633e7f74ba0b08e424ea2
Martin Gondermann <magicmonty@pagansoft.de>
2015-08-27 15:58:17 +0200

    Implements OptionValues with default value

--- Option/EnumerableWithOptionExtensions.cs
+++ Option/EnumerableWithOptionExtensions.cs
@@ -16,6 +16,19 @@ namespace Pagansoft.Functional
             foreach (var option in options.Where(o => o.IsSome))
                 yield return option.Value;
         }
+
+        /// <summary>
+        /// Returns the values from all elements with all nones replaced by the given <paramref name="defaultValue"/>,
+        /// </summary>
+        /// <returns>The values of all elements with a value.</returns>
+        /// <param name="options">The list of options.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public static IEnumerable<T> OptionValues<T>(this IEnumerable<Option<T>> options, T defaultValue)
+        {
+            foreach (var option in options)
+                yield return option.ReturnValueOr(defaultValue);
+        }
     }
 }
 
--- Test/EnumerableWithOptionExtensionsTest.cs
+++ Test/EnumerableWithOptionExtensionsTest.cs
@@ -21,6 +21,14 @@ namespace Pagansoft.Functional
 
             options.OptionValues().ShouldBe(new [] { 10, 20 });
         }
+
+        [Test]
+        public void OptionValues_With_Default_Value_Returns_Nones_Replaced_With_Given_Value()
+        {
+            var options = new Option<int>[] { Option.Some(10), Option.None<int>(), Option.Some(20) };
+
+            options.OptionValues(42).ShouldBe(new [] { 10, 42, 20 });
+        }
     }
 }
 
