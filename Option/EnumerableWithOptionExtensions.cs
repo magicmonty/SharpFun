54e785171363378a3d10d04b79a0da1b5d70697e
Martin Gondermann <magicmonty@pagansoft.de>
2015-08-27 15:53:20 +0200

    Implements extension method "OptionValues"

---
+++ Option/EnumerableWithOptionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace Pagansoft.Functional
+{
+    public static class EnumerableWithOptionExtensions
+    {
+        /// <summary>
+        /// Returns the values from all elements with a value
+        /// </summary>
+        /// <returns>The values of all elements with a value.</returns>
+        /// <param name="options">The list of options.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public static IEnumerable<T> OptionValues<T>(this IEnumerable<Option<T>> options)
+        {
+            foreach (var option in options.Where(o => o.IsSome))
+                yield return option.Value;
+        }
+    }
+}
+
---
+++ Test/EnumerableWithOptionExtensionsTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace Pagansoft.Functional
+{
+    [TestFixture]
+    public class EnumerableWithOptionExtensionsTest
+    {
+        [Test]
+        public void OptionValues_Is_Empty_If_Enumerable_Is_Empty()
+        {
+            var options = new Option<int>[] { };
+
+            options.OptionValues().ShouldBeEmpty();
+        }
+
+        [Test]
+        public void OptionValues_Returns_Only_Values_From_Somes()
+        {
+            var options = new Option<int>[] { Option.Some(10), Option.None<int>(), Option.Some(20) };
+
+            options.OptionValues().ShouldBe(new [] { 10, 20 });
+        }
+    }
+}
+
