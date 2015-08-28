6c71298361e7ad63dc3c62a5d739b4e330fb65ff
Martin Gondermann <magicmonty@pagansoft.de>
2015-08-28 08:30:39 +0200

    Adds clean target to build.fsx

--- build.fsx
+++ build.fsx
@@ -2,10 +2,21 @@
 #r @"packages/FAKE/tools/FakeLib.dll"
 open Fake
 
-// Default target
+// Properties
+let buildDir = "./build/"
+
+// Targets
+Target "Clean" (fun _ ->
+  CleanDir buildDir
+)
+
 Target "Default" (fun _ ->
   trace "Hello World from FAKE"
 )
+
+// Dependencies
+"Clean"
+  ==> "Default"
 
 // start build
 RunTargetOrDefault "Default"
