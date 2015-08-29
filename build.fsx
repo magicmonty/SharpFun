9040d8971f8f061fd72e92cc4dbac98eb1f13678
Martin Gondermann <magicmonty@pagansoft.de>
2015-08-29 11:38:38 +0200

    Formatts build.fsx

--- build.fsx
+++ build.fsx
@@ -11,28 +11,45 @@ let testDir = buildDir @@ "test"
 let deployDir = buildDir @@ "deploy"
 
 // Targets
-Target "Clean" (fun _ -> CleanDir buildDir)
-Target "Default" (fun _ -> trace "Hello World from FAKE")
-Target "BuildRelease" (fun _ -> 
+Target "Clean" (fun _ ->
+  CleanDir buildDir
+)
+
+Target "Default" (fun _ ->
+  trace "Build successful"
+)
+
+Target "BuildRelease" (fun _ ->
     !!"*.sln"
     |> MSBuildRelease releaseDir "Build"
     |> Log "ReleaseBuild-Output")
-Target "BuildDebug" (fun _ -> 
+
+Target "BuildDebug" (fun _ ->
     !!"*.sln"
     |> MSBuildDebug testDir "Build"
     |> Log "DebugBuild-Output: ")
-Target "Test" (fun _ -> 
-    !!(testDir @@ "Test.dll") |> NUnit(fun p -> 
-                                     { p with DisableShadowCopy = true
-                                              OutputFile = testDir @@ "TestResults.xml" }))
-Target "CreatePackage" (fun _ -> 
-    Pack(fun p -> 
+
+Target "Test" (fun _ ->
+    !!(testDir @@ "Test.dll")
+      |> NUnit(fun p ->
+          { p with DisableShadowCopy = true
+                   OutputFile = testDir @@ "TestResults.xml" }))
+
+Target "CreatePackage" (fun _ ->
+    Pack(fun p ->
         { p with OutputPath = deployDir
                  TemplateFile = "Functional/Functional.paket.template"
                  WorkingDir = "Functional"
                  Version = GetAssemblyVersionString(releaseDir @@ "Pagansoft.Functional.dll")
                  ToolPath = ".paket/paket.exe" }))
+
 // Dependencies
-"Clean" ==> "BuildDebug" ==> "Test" ==> "BuildRelease" ==> "CreatePackage" ==> "Default"
+"Clean"
+  ==> "BuildDebug"
+  ==> "Test"
+  ==> "BuildRelease"
+  ==> "CreatePackage"
+  ==> "Default"
+
 // start build
 RunTargetOrDefault "Default"
