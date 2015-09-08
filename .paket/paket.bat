d584c908d2e15eeb378cf80b21528d38d0b6e2ef
Martin Gondermann <magicmonty@users.noreply.github.com>
2015-09-08 07:38:58 +0200

    Update paket.bat

--- .paket/paket.bat
+++ .paket/paket.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-%~dp0paket.bootstrapper.exe --self --prefer-nuget
-%~dp0paket.bootstrapper.exe --prefer-nuget
+%~dp0paket.bootstrapper.exe --self
+%~dp0paket.bootstrapper.exe
 %~dp0paket.exe %1 %2 %3 %4 %5 %6 %7 %8
